e90a612bf6df11fc5bb77f860834df97c4ebf6fa
Vincent Lin <vinlin24@outlook.com>
2023-03-30 04:53:22 -0700

    Make minor additions to __init__.py

--- src/strutils/__init__.py
+++ src/strutils/__init__.py
@@ -1,11 +1,16 @@
 """
 # strutils
 
-A collection of scripts intended to be added to the user's PATH.  The
+A collection of scripts intended to be added to the user's PATH.  They
 are intended as Unix-like interfaces to Python's string manipulation
 utilities.  They are organized as a package for installation purposes
 and are not intended to be imported from.
 """
 
 __author__ = "Vincent Lin"
+
+# This is a dummy version string and is not even compliant with semantic
+# versioning.  Unless and until I decide to upload this to PyPI, this
+# will remain the same value and not be updated despite changes in the
+# source code.
 __version__ = "0.0.0"
