6af675d1a0e630a92222c1d2d533edf44130bfd4
Vincent Lin <vinlin24@outlook.com>
2023-03-30 03:27:36 -0700

    Add --tabs/--indent options to snippet

--- src/strutils/snippet.py
+++ src/strutils/snippet.py
@@ -7,26 +7,51 @@ can be pasted into the "body" field of a VS Code snippets file.
 EXAMPLES:
 
     $ cat body.txt | snippet
-    $ ./snippet body.txt
+    $ ./snippet body.txt --indent 2
 """
 
 import json
 import sys
-from argparse import ArgumentParser, RawTextHelpFormatter
+from argparse import ArgumentParser, ArgumentTypeError, RawTextHelpFormatter
 from pathlib import Path
 
 __author__ = "Vincent Lin"
+
+
+def valid_num_spaces_indent(value: str) -> int:
+    as_int = int(value)
+    valid_range = range(0, 17)
+    if as_int not in valid_range:
+        raise ArgumentTypeError(f"{value} is not in {valid_range}.")
+    return as_int
+
 
 parser = ArgumentParser(prog=Path(sys.argv[0]).name,
                         description=__doc__,
                         formatter_class=RawTextHelpFormatter)
 parser.add_argument("file_path", metavar="FILE", nargs="?", type=Path,
                     help="file with raw snippet; read from stdin if omitted")
+indentation_group = parser.add_mutually_exclusive_group()
+indentation_group.add_argument("-i", "--indent", type=valid_num_spaces_indent,
+                               help="number of spaces to use as indentation")
+indentation_group.add_argument("-t", "--tabs", action="store_true",
+                               help="use tabs for indentation")
 
 
 def main() -> None:
     namespace = parser.parse_args()
+
     file_path: Path | None = namespace.file_path
+    indent: int | None = namespace.indent
+    tabs: bool = namespace.tabs
+
+    if tabs:
+        indentation = "\t"
+    elif indent is not None:
+        indentation = " " * indent
+    # Default to 4 spaces if no indentation strategy provided.
+    else:
+        indentation = " " * 4
 
     if file_path is None:
         source = sys.stdin
@@ -39,7 +64,7 @@ def main() -> None:
     print("[")
     for line in input_lines:
         escaped = json.dumps(line)
-        print(f"  {escaped},")
+        print(f"{indentation}{escaped},")
     print("]")
 
 
