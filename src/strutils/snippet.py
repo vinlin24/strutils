650b2b0fb65a2533aecad3760d8b4ad81b53755a
Vincent Lin <vinlin24@outlook.com>
2023-03-30 03:15:34 -0700

    Port snippet to argparse

--- src/strutils/snippet.py
+++ src/strutils/snippet.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""snippet
-
-Convert the contents of a text file into a JSON array of strings that
+"""
+Convert the contents of a text stream into a JSON array of strings that
 can be pasted into the "body" field of a VS Code snippets file.
 
-USAGE: `cat body.txt | ./snippet`
+EXAMPLES:
 
--OR-
-
-USAGE: `./snippet body.txt`
+    $ cat body.txt | snippet
+    $ ./snippet body.txt
 """
 
 import json
 import sys
+from argparse import ArgumentParser, RawTextHelpFormatter
+from pathlib import Path
 
 __author__ = "Vincent Lin"
 
+parser = ArgumentParser(prog=Path(sys.argv[0]).name,
+                        description=__doc__,
+                        formatter_class=RawTextHelpFormatter)
+parser.add_argument("file_path", metavar="FILE", nargs="?", type=Path,
+                    help="file with raw snippet; read from stdin if omitted")
+
 
 def main() -> None:
-    try:
-        file_path = sys.argv[1]
-        source = open(file_path, "rt", encoding="utf-8")
-    except IndexError:
+    namespace = parser.parse_args()
+    file_path: Path | None = namespace.file_path
+
+    if file_path is None:
         source = sys.stdin
+    else:
+        source = file_path.open("rt", encoding="utf-8")
 
     with source:
         input_lines = source.read().splitlines()
