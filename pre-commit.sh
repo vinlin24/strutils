3062ce7f2f9b54c459e9984cc656b4d240189a5c
Vincent Lin <vinlin24@outlook.com>
2024-07-28 00:04:27 -0400

    chore: add pre-commit hook
    This automates running the README.md updater script.

--- pre-commit.sh
+++ pre-commit.sh
@@ -4,9 +4,15 @@
 repo_root="$(git rev-parse --show-toplevel)"
 cd "$repo_root" || exit 1
 
-if ! make readme; then
+make_output="$(make readme)"
+make_status=$?
+echo "$make_output"
+
+if [ $make_status -ne 0 ]; then
     echo >&2 'failed to update README.md, aborting.'
     exit 1
 fi
 
-git add README.md
+if ! echo "$make_output" | grep --quiet 'Nothing to be done for'; then
+    git add README.md
+fi
