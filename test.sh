3b599ed1ef32907e65bc4e57c4b925a860973794
Vincent Lin <vinlin24@outlook.com>
2024-07-28 12:18:02 -0400

    chore: support -v/--verbose option in test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
-# USAGE: ./test.sh [PATTERN]
+# USAGE: ./test.sh [-v|--verbose] [PATTERN]
 
 self="$(basename "$0")"
 script_dir="$(dirname "$0")"
 test_dir="${script_dir}/test"
-pattern="$1"
+
+POSITIONAL_ARGS=()
+VERBOSE=0
+
+while [ $# -gt 0 ]; do
+    case $1 in
+    -v | --verbose)
+        VERBOSE=1
+        shift
+        ;;
+    *)
+        POSITIONAL_ARGS+=("$1")
+        shift
+        ;;
+    esac
+done
+
+verbosity_flag=''
+if [ $VERBOSE -eq 1 ]; then
+    verbosity_flag=--verbose
+fi
+pattern="${POSITIONAL_ARGS[0]}"
 
 if [ -n "$pattern" ]; then
     echo "$self: running only tests whose name contains: $pattern"
-    python3 -m unittest discover --start-directory "$test_dir" -k "$pattern"
+    python3 -m unittest discover \
+        $verbosity_flag --start-directory "$test_dir" -k "$pattern"
 else
     echo "$self: running ALL tests under $test_dir"
-    python3 -m unittest discover --start-directory "$test_dir"
+    python3 -m unittest discover \
+        $verbosity_flag --start-directory "$test_dir"
 fi
