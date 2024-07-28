ce0ec6d5f11e946908aa8f9444b42eb634fd083b
Vincent Lin <vinlin24@outlook.com>
2024-07-27 20:52:38 -0400

    test: add base class helper for shared code

--- test/common.py
+++ test/common.py
@@ -4,10 +4,42 @@ Code to share among tests.
 """
 
 import subprocess
+import unittest
+from typing import NamedTuple
 
 
-def get_output(script: str) -> str:
-    stdout = subprocess.check_output(script, shell=True)
-    as_str = stdout.decode()
-    normalized = as_str.replace("\r\n", "\n")
-    return normalized
+class ProcessResult(NamedTuple):
+    stdout: str
+    stderr: str
+    exit_code: int
+
+
+class TestBase(unittest.TestCase):
+    def run_command(
+        self,
+        script: str,
+        *,
+        stdin: str | None = None,
+    ) -> ProcessResult:
+        process = subprocess.run(
+            script,
+            shell=True,
+            capture_output=True,
+            input=stdin,
+            text=True,
+            check=False,
+        )
+        return ProcessResult(process.stdout, process.stderr, process.returncode)
+
+    def assert_success(
+        self,
+        process_result: ProcessResult,
+        expected_stdout: str,
+        *,
+        stderr_ok: bool = False,
+    ) -> None:
+        stdout, stderr, exit_code = process_result
+        self.assertEqual(exit_code, 0)
+        self.assertEqual(stdout, expected_stdout)
+        if not stderr_ok:
+            self.assertEqual(stderr, "")
--- test/test_chr.py
+++ test/test_chr.py
@@ -7,22 +7,22 @@ Unit tester for the chr program.
 
 import unittest
 
-from common import get_output
+from common import TestBase
 
 
-class TestChr(unittest.TestCase):
+class TestChr(TestBase):
     def test_basic(self) -> None:
-        output = get_output("chr 65 66 67")
-        self.assertEqual(output, "A B C\n")
+        result = self.run_command("chr 65 66 67")
+        self.assert_success(result, "A B C\n")
 
     def test_echo(self) -> None:
-        output = get_output("chr -e 0x50 0x52 0x4F")
-        self.assertEqual(output, "80 82 79\nP  R  O \n")
+        result = self.run_command("chr -e 0x50 0x52 0x4F")
+        self.assert_success(result, "80 82 79\nP  R  O \n")
 
     def test_delim(self) -> None:
         nums = " ".join(str(num) for num in range(68, 76))
-        output = get_output(f"chr -d _ {nums}")
-        self.assertEqual(output, "D_E_F_G_H_I_J_K\n")
+        result = self.run_command(f"chr -d _ {nums}")
+        self.assert_success(result, "D_E_F_G_H_I_J_K\n")
 
 
 if __name__ == "__main__":
