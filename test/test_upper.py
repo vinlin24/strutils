de12340aa31fb2f4f41d678f23f679fd9c847cc6
Vincent Lin <vinlin24@outlook.com>
2024-07-30 20:56:27 -0400

    test: add more comprehensive tests for upper

--- test/test_upper.py
+++ test/test_upper.py
@@ -10,12 +10,28 @@ from common import TestBase
 
 class TestUpper(TestBase):
     def test_basic(self) -> None:
+        result = self.run_command('upper "hello there"')
+        self.assert_success(result, "HELLO THERE")
+
+    def test_stdin(self) -> None:
+        result = self.run_command("upper", stdin="hello there")
+        self.assert_success(result, "HELLO THERE")
+
+    def test_multiple_tokens(self) -> None:
+        result = self.run_command("upper hello there  general   kenobi")
+        self.assert_success(result, "HELLO THERE GENERAL KENOBI")
+
+    def test_trailing_newline(self) -> None:
         result = self.run_command('upper -n "hello there"')
         self.assert_success(result, "HELLO THERE\n")
 
     def test_title_case(self) -> None:
-        result = self.run_command("upper -t", stdin=" hello\n    there\n")
-        self.assert_success(result, " Hello\n    There\n")
+        result = self.run_command('upper -t "hello there"')
+        self.assert_success(result, "Hello There")
+
+    def test_title_case_with_arbitrary_whitespace(self) -> None:
+        result = self.run_command("upper -t", stdin=" hello\n  \t there\n  ")
+        self.assert_success(result, " Hello\n  \t There\n  ")
 
     def test_force_title_case(self) -> None:
         result = self.run_command("upper -tf", stdin="hello THERE\n")
@@ -25,5 +41,12 @@ class TestUpper(TestBase):
         result = self.run_command(
             'upper -td "-"',
             stdin="hello-there-general-kenobi\n",
+        )
+        self.assert_success(result, "Hello-There-General-Kenobi\n")
+
+    def test_force_title_case_with_delimiter(self) -> None:
+        result = self.run_command(
+            'upper -tfd "-"',
+            stdin="hello-THERE-General-kENOBi\n",
         )
         self.assert_success(result, "Hello-There-General-Kenobi\n")
